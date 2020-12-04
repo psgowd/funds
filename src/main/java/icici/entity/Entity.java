11860f6498336fd5a63184adaec7d95eaa2e5c27
psgowd <psgowd28@gmail.com>
2020-12-04 07:32:49 +0530

    .war file funds

---
+++ src/main/java/icici/entity/Entity.java
@@ -0,0 +1,8 @@
+package icici.entity;
+
+public abstract class Entity<T> {
+
+    public abstract T getId();
+
+
+}
---
+++ src/test/java/icici/resource/JobResourceTest.java
@@ -0,0 +1,8 @@
+package icici.resource;
+
+
+public class JobResourceTest {
+
+
+
+}
